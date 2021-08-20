d0d88de492728f7083462e84936d89dcb21a7ca0
LAPTOP-774063JM\ASUS <boatsuban2543@gmail.com>
2021-08-20 16:41:18 +0700

    Add post & delete

--- index.js
+++ index.js
@@ -45,9 +45,84 @@ app.post('/plus', function (req, res) {
 	var num1 = req.body.num1;
 	var num2 = req.body.num2;
 
-	res.send('{"result": '+ (num1+num2) +'}');
+	res.send('{"result": ' + (num1 + num2) + '}');
 
 });
+
+
+
+
+app.post('/book',  function (req, res){
+
+	var author = req.body.author;
+	var bookid = Number(req.body.bookid);
+	var category = req.body.category;
+	var isbn = req.body.isbn;
+	var pageCount = Number(req.body.pageCount);
+	var price = Number(req.body.price);
+	var publishedDate = req.body.publishedDate;
+	var shortDescription = req.body.shortDescription;
+	var thumbnailUrl = req.body.thumbnailUrl;
+	var title = req.body.title;
+
+	//console.log(author);
+
+	var referencaPath ='/books/' + bookid + '/';
+
+	var booksReference = db.ref(referencaPath);
+
+	if (booksReference != null){
+
+		booksReference.update( {author:author, bookid: bookid, category: category,
+			 isbn: isbn, pageCount: pageCount, price: price, publishedDate: publishedDate, 
+			 shortDescription: shortDescription, thumbnailUrl: thumbnailUrl, title: title,  }  ,
+			 function(error){
+				 if (error){
+					 res.send("Data could not be saved." + error)
+				 }
+				 else{
+					res.send("Success!");
+				 }
+			 }
+			 );
+
+
+	}
+
+
+});
+
+
+
+
+//rectandgle (post)
+app.post('/rectandgle', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	var size1 = req.body.size1;
+	var size2 = req.body.size2;
+
+	res.send('{ "result ": ' + (size1 * size2) + '}');
+
+});
+
+
+
+
+
+//circle (post)
+app.post('/circle', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	var radius = req.body.radius1;
+
+	res.send('{ "result ": ' + (3.14 * radius * radius) + '}');
+
+});
+
+
+
+
 
 
 
@@ -111,10 +186,49 @@ app.get('/book/:bookid', function (req, res) {
 
 });
 
+app.get('/student/:studentId', function (req, res) {
+
+	//Code Here
+	res.setHeader('Content-Type', 'application/json');
+	var studentId = req.params.studentId;
+
+	var booksReference = db.ref("students");
+
+	//Attach an asynchronous callback to read the data
+	booksReference.orderByChild("studentId").equalTo(studentId).on("child_added",
+		function (snapshot) {
+			res.json(snapshot.val());
+			booksReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
+});
+
+
+
+
+
+
+
+
+
+
+
 app.delete('/book/:bookid', function (req, res) {
 
 	//Code Here
+	//res.setHeader('Content-Type', 'application/json');
+	var bookid = Number(req.params.bookid);
 
+	var referencaPath ='/books/' + bookid + '/';
+	var booksReference = db.ref(referencaPath);
+
+	if(booksReference!=null){
+		booksReference.remove()
+		res.send("Sucess!")
+	}
+	if (error) throw error;
 
 
 });
